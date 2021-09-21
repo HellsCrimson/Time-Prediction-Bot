be394ed3634b28bd217e48af60b04d67a27f398f
matthias.rauline <matthias.rauline@epita.fr>
2021-09-22 00:03:27 +0200

    help command made + run script made

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,19 @@
+const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const process_prediction = require('../process-prediction.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Get help'),
 	async execute(interaction) {
-			await interaction.reply(`Here is your help coming`);
+		const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+		let help = "";
+
+		for (const file of commandFiles) {
+			const command = require(`./${file}`);
+			help += `${command.data.name} -> ${command.data.description}\n`;
+		}
+
+		await interaction.reply({content: `Here is your help:\n${help}`, ephemeral: true});
 	},
 };
